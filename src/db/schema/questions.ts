e71af18f7d63bdd79d60a885073e364704522cdc
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2025-07-12 11:17:02 -0300

    feat: add create question, create room and get room questions

---
+++ src/db/schema/questions.ts
@@ -0,0 +1,13 @@
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { rooms } from "./rooms.ts";
+
+export const questions = pgTable("questions", {
+  id: uuid().primaryKey().defaultRandom(),
+  question: text().notNull(),
+  answer: text(),
+
+  roomId: uuid()
+    .references(() => rooms.id)
+    .notNull(),
+  createdAt: timestamp().defaultNow().notNull(),
+});
